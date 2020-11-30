aec7814e77336a7b6a35afca697d5328fe706543
Alexander Sidorov <alexander@sidorov.dev>
2020-12-01 02:09:26 +0300

    deploy on Heroku

--- src/scripts/gunicorn.conf.py
+++ src/scripts/gunicorn.conf.py
@@ -3,19 +3,6 @@ from multiprocessing import cpu_count
 from framework.dirs import DIR_SRC
 from framework.util.settings import get_setting
 
-# try:
-#     from pathlib import Path
-#
-#     _this_dir = Path(__file__).parent.resolve().as_posix()
-#     import sys
-#
-#     sys.path.append(_this_dir)
-#
-#     from consts import DIR_SRC
-#     from utils import get_setting
-# finally:
-#     print("[done] reading Gunicorn config")
-
 _port = get_setting("PORT", 8000, convert=int)
 bind = f"0.0.0.0:{_port}"
 chdir = DIR_SRC.as_posix()
