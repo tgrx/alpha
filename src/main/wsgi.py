8b01f9f10664868ece3da304cb1c77fb800525c9
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-11-30 16:02:29 -0800

    Heroku (#9)
    * deploy on Heroku
    
    * implemented configuring Heroku by API
    
    * upgraded packages
    
    * integrated sentry

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,4 +1,14 @@
+import sentry_sdk
+
+from framework.util.settings import get_setting
+
+sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
+
+
 def application(environ, start_response):
+    if environ["PATH_INFO"] == "/e/":
+        division = 1 / 0
+
     status = "200 OK"
 
     headers = {
