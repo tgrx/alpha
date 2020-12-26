100960d7005028d43b4aef5952c6a8545e102e13
Alexander Sidorov <alexander@sidorov.dev>
2020-12-26 18:16:48 +0300

    Hotfixes (#20)
    * script `print_db_config.py` is fixed
    
    invalid imports, as usual
    
    * File `src/main/settings.py` is not needed anymore
    
    * DIR_TEST_ARTIFACTS points to valid directory
    
    * attempt 1: CI containers are upgraded
    
    * attempt 2: CI containers are upgraded

--- src/framework/dirs.py
+++ src/framework/dirs.py
@@ -11,5 +11,5 @@ DIR_TESTS = (DIR_REPO / "tests").resolve()
 DIR_FRAMEWORK = (DIR_SRC / "framework").resolve()
 DIR_SCRIPTS = (DIR_SRC / "scripts").resolve()
 
-DIR_TEST_ARTIFACTS = (DIR_REPO / ".test_artifacts").resolve()
+DIR_TEST_ARTIFACTS = (DIR_REPO / ".tests_artifacts").resolve()
 DIR_TEST_ARTIFACTS.mkdir(exist_ok=True)
