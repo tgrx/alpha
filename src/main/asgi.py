1bd2696b51a03c2f0b8ac0fda1222b1787b13a8d
Alexander Sidorov <alexander@sidorov.dev>
2021-07-14 19:04:22 +0300

    main app is refactored

--- src/main/asgi.py
+++ src/main/asgi.py
@@ -1,4 +1,3 @@
-import json
 from typing import Callable
 from typing import Dict
 
@@ -6,6 +5,11 @@ import sentry_sdk
 
 from framework.config import settings
 from framework.logging import get_logger
+from main.payload import HostPortT
+from main.payload import PayloadT
+from main.payload import RequestT
+from main.payload import ScopeAsgiT
+from main.payload import ScopeT
 
 sentry_sdk.init(settings.SENTRY_DSN, traces_sample_rate=1.0)
 
@@ -13,6 +17,9 @@ logger = get_logger("asgi")
 
 
 async def application(scope: Dict, receive: Callable, send: Callable):
+    if scope["type"] == "lifespan":
+        return
+
     path = scope["path"]
     logger.debug(f"path: {path}")
 
@@ -25,21 +32,51 @@ async def application(scope: Dict, receive: Callable, send: Callable):
 
     await send(
         {
-            "type": "http.response.start",
-            "status": 200,
             "headers": [
                 [b"content-type", b"application/json"],
             ],
+            "status": 200,
+            "type": "http.response.start",
         }
     )
 
-    payload = {}
+    payload = build_payload(scope, request)
 
     await send(
         {
+            "body": payload.json(sort_keys=True, indent=2).encode(),
             "type": "http.response.body",
-            "body": json.dumps(payload).encode(),
         }
     )
 
-    logger.debug(f"response has been sent")
+    logger.debug("response has been sent")
+
+
+def build_payload(scope: Dict, request: Dict) -> PayloadT:
+    payload = PayloadT(
+        request=RequestT(
+            body=request["body"].decode(),
+            more_body=request["more_body"],
+            type=request["type"],
+        ),
+        scope=ScopeT(
+            asgi=ScopeAsgiT.parse_obj(scope["asgi"]),
+            client=HostPortT.parse_obj(
+                dict(zip(["host", "port"], scope["client"]))
+            ),
+            headers={k.decode(): v.decode() for k, v in scope["headers"]},
+            http_version=scope["http_version"],
+            method=scope["method"],
+            path=scope["path"],
+            query_string=scope["query_string"].decode(),
+            raw_path=scope["raw_path"].decode(),
+            root_path=scope["root_path"],
+            scheme=scope["scheme"],
+            server=HostPortT.parse_obj(
+                dict(zip(["host", "port"], scope["server"]))
+            ),
+            type=scope["type"],
+        ),
+    )
+
+    return payload
--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,27 +1,71 @@
 import httpx
 import pytest
 
+from main.payload import PayloadT
+
 pytestmark = [
     pytest.mark.asyncio,
     pytest.mark.functional,
 ]
 
 
-def validate_root_response(resp: httpx.Response):
-    assert resp.status_code == 200
-    assert resp.json() == {}
-
-
 async def test_asgi_app(asgi_client: httpx.AsyncClient):
     resp: httpx.Response = await asgi_client.get("/")
-    validate_root_response(resp)
+    payload = PayloadT.parse_obj(resp.json())
+
+    assert payload.request
+    assert payload.request.body == ""
+    assert payload.request.more_body is True
+    assert payload.request.type == "http.request"
+
+    assert payload.scope
+    assert payload.scope.asgi.spec_version is None
+    assert payload.scope.asgi.version == "3.0"
+    assert payload.scope.client.host == "127.0.0.1"
+    assert payload.scope.client.port in range(1, 65535)
+    assert payload.scope.headers["host"] == "asgi"
+    assert payload.scope.headers["user-agent"] == "python-httpx/0.18.2"
+    assert payload.scope.http_version == "1.1"
+    assert payload.scope.method == "GET"
+    assert payload.scope.path == "/"
+    assert payload.scope.query_string == ""
+    assert payload.scope.raw_path == "/"
+    assert payload.scope.root_path == ""
+    assert payload.scope.scheme == "http"
+    assert payload.scope.server.host == "asgi"
+    assert payload.scope.server.port is None
+    assert payload.scope.type == "http"
 
 
 @pytest.mark.webapp
 async def test_web_app(web_client: httpx.AsyncClient):
     try:
         resp: httpx.Response = await web_client.get("/")
-        validate_root_response(resp)
+        payload = PayloadT.parse_obj(resp.json())
+
+        assert payload.request
+        assert payload.request.body == ""
+        assert payload.request.more_body is False
+        assert payload.request.type == "http.request"
+
+        assert payload.scope
+        assert payload.scope.asgi.spec_version == "2.1"
+        assert payload.scope.asgi.version == "3.0"
+        assert payload.scope.client.host == "127.0.0.1"
+        assert payload.scope.client.port in range(1000, 65535)
+        assert payload.scope.headers["host"] == "localhost:8000"
+        assert payload.scope.headers["user-agent"] == "python-httpx/0.18.2"
+        assert payload.scope.http_version == "1.1"
+        assert payload.scope.method == "GET"
+        assert payload.scope.path == "/"
+        assert payload.scope.query_string == ""
+        assert payload.scope.raw_path == "/"
+        assert payload.scope.root_path == ""
+        assert payload.scope.scheme == "http"
+        assert payload.scope.server.host == "127.0.0.1"
+        assert payload.scope.server.port == 8000
+        assert payload.scope.type == "http"
+
     except (httpx.ConnectError, httpx.TimeoutException) as err:
         raise AssertionError(
             f"unable to connect to server @ {web_client.base_url}"
