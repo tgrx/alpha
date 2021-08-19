047493a290f8458ab59ec0bab6cb72dfb884463d
Alexander Sidorov <alexander@sidorov.dev>
2021-08-19 04:03:27 +0300

    fixed containers restart on C-c

--- src/framework/dirs.py
+++ src/framework/dirs.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 _this_file = Path(__file__).resolve()
@@ -5,7 +6,7 @@ _this_file = Path(__file__).resolve()
 DIR_REPO = _this_file.parent.parent.parent.resolve()
 
 DIR_CONFIG = (DIR_REPO / "config").resolve()
-DIR_CONFIG_SECRETS = DIR_CONFIG / ".secrets"
+DIR_CONFIG_SECRETS = Path(os.getenv("SECRETS_DIR", DIR_CONFIG / ".secrets"))
 DIR_CONFIG_SECRETS.mkdir(exist_ok=True)
 
 DIR_IDEA = (DIR_REPO / ".idea").resolve()
