b6ccdba05b03b8f56d0b8149fcd1a49d94172443
Alexander Sidorov <alexander@sidorov.dev>
2023-02-20 14:56:13 +0300

    gunicorn added

--- services/webapp/config/gunicorn.conf.py
+++ services/webapp/config/gunicorn.conf.py
@@ -1,14 +1,14 @@
-from alpha.dirs import DIR_SRC
+from alpha.dirs import DIR_APP
 from alpha.settings import Settings
 
 settings = Settings()
 
 bind = f"0.0.0.0:{settings.PORT}"
-chdir = DIR_SRC.as_posix()
+chdir = DIR_APP.as_posix()
 graceful_timeout = settings.REQUEST_TIMEOUT
 max_requests = 200
 max_requests_jitter = 20
-pythonpath = DIR_SRC.as_posix()
+pythonpath = DIR_APP.as_posix()
 reload = False
 timeout = graceful_timeout * 2
 worker_class = "uvicorn.workers.UvicornH11Worker"
